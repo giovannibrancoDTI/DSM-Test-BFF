f8b117f0d0831dce266e57e8b9bbcaf8bd5b29db
giovannibrancoDTI <giovanni.branco@dtidigital.com.br>
2025-04-28 21:26:42 -0300

    feat: adding create, update and delete methods

--- src/repositories/PhotoRepository.ts
+++ src/repositories/PhotoRepository.ts
@@ -12,6 +12,35 @@ class PhotoRepository implements IPhotoRepository {
 
     return response.data as PhotoModel[];
   }
+
+  async createPhoto(photo: PhotoModel): Promise<PhotoModel> {
+    const response = await axios.post(
+      `https://jsonplaceholder.typicode.com/photos`,
+      photo,
+    );
+
+    if (response.status !== 201) throw new Error("Failed to create photo");
+
+    return response.data as PhotoModel;
+  }
+
+  async deletePhoto(photoId: number): Promise<void> {
+    const response = await axios.delete(
+      `https://jsonplaceholder.typicode.com/photos/${photoId}`,
+    );
+
+    if (response.status !== 200) throw new Error("Failed to delete photo");
+  }
+  async updatePhoto(photo: PhotoModel): Promise<PhotoModel> {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/photos/${photo.id}`,
+      photo,
+    );
+
+    if (response.status !== 200) throw new Error("Failed to update photo");
+
+    return response.data as PhotoModel;
+  }
 }
 
 export default new PhotoRepository();
--- src/services/AlbumService.ts
+++ src/services/AlbumService.ts
@@ -1,15 +1,48 @@
+import { AlbumModel } from "@/domain/AlbumModel";
 import AlbumRepository from "../repositories/AlbumRepository";
 import { IAlbumRepository } from "../repositories/interfaces/IAlbumRepository";
 import { IAlbumService } from "./interfaces/IAlbumService";
+import { IPhotoRepository } from "@/repositories/interfaces/IPhotoRepository";
+import PhotoRepository from "@/repositories/PhotoRepository";
 
 class AlbumService implements IAlbumService {
-  constructor(private albumRepository: IAlbumRepository) {}
+  constructor(
+    private albumRepository: IAlbumRepository,
+    private photoRepository: IPhotoRepository,
+  ) {}
 
   async listAlbumsByUserId(userId: number) {
     if (!userId) throw new Error("User ID is required");
 
     return await this.albumRepository.getAlbumsByUserId(userId);
   }
+
+  async createAlbum(album: { userId: number; title: string }) {
+    if (!album.userId || !album.title) {
+      throw new Error("User ID and title are required");
+    }
+
+    return await this.albumRepository.createAlbum(album);
+  }
+
+  async deleteAlbum(albumId: number) {
+    if (!albumId) throw new Error("Album ID is required");
+
+    const photos = await this.photoRepository.getPhotosByAlbumId(albumId);
+
+    photos.forEach(async (photo) => {
+      await this.photoRepository.deletePhoto(photo.id);
+    });
+
+    return await this.albumRepository.deleteAlbum(albumId);
+  }
+  async updateAlbum(album: AlbumModel) {
+    if (!album.id || !album.userId || !album.title) {
+      throw new Error("ID, User ID, and title are required");
+    }
+
+    return await this.albumRepository.updateAlbum(album);
+  }
 }
 
-export default new AlbumService(AlbumRepository);
+export default new AlbumService(AlbumRepository, PhotoRepository);
